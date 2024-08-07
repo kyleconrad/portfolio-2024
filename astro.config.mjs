b8a6ba36e1d829920ade76a937b970bdf9282c6d
Kyle Conrad <kyle@kyleconrad.com>
2024-08-07 09:25:05 -0400

    Adding compression to build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kyleconrad.com',
-  integrations: [sitemap()]
+  integrations: [
+    sitemap(),
+    (await import('@playform/compress')).default({
+			SVG: false,
+		}),
+  ]
 });
