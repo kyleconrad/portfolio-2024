7ad937b89d2760ab7f5400139e8d83239ff04d7c
Kyle Conrad <kyle@kyleconrad.com>
2024-09-18 21:25:32 -0400

    Adding deployment tasks

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 source .env
 
-# aws s3 sync --delete ./dist/ s3://$AWS_BUCKET && aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --paths "/*"
+aws s3 sync --delete ./dist/ s3://$AWS_BUCKET && aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --paths "/*"
